03ace561cdd69afe12a7765867ebed93f0ac6bf2
Luan Felipe <luandhx@gmail.com>
2023-10-12 19:56:07 -0300

    Add função desocuparVaga, verificações e estilos

--- Controller/script.js
+++ Controller/script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function() {
     // Iniciando o estacionamento
     const estacionamento = new Estacionamento(20);
 
@@ -7,52 +7,52 @@ document.addEventListener("DOMContentLoaded", function() {
     preencherListaVagas(estacionamento);
 
     // Definindo variaveis para as páginas
-    const cadastroSection = document.getElementById("cadastroForm");
-    const listagemSection = document.getElementById("listagemReservas");
-    const vagasSection = document.getElementById("vagasDisponiveis");
+    const cadastroSection = document.getElementById('cadastroForm');
+    const listagemSection = document.getElementById('listagemReservas');
+    const vagasSection = document.getElementById('vagasDisponiveis');
 
     // Inicialmente, exiba a seção de cadastro e oculte as outras
-    cadastroSection.style.display = "block";
-    listagemSection.style.display = "none";
-    vagasSection.style.display = "none";
+    cadastroSection.style.display = 'block';
+    listagemSection.style.display = 'none';
+    vagasSection.style.display = 'none';
 
-    const cadastroLink = document.querySelector("nav ul li:nth-child(1) a");
-    const listagemLink = document.querySelector("nav ul li:nth-child(2) a");
-    const vagasLink = document.querySelector("nav ul li:nth-child(3) a");
+    const cadastroLink = document.querySelector('nav ul li:nth-child(1) a');
+    const listagemLink = document.querySelector('nav ul li:nth-child(2) a');
+    const vagasLink = document.querySelector('nav ul li:nth-child(3) a');
 
     // Adicionando funcionalidade ao menu de navegacao
-    cadastroLink.addEventListener("click", function(event) {
+    cadastroLink.addEventListener('click', function(event) {
         event.preventDefault();
-        cadastroSection.style.display = "block";
-        listagemSection.style.display = "none";
-        vagasSection.style.display = "none";
+        cadastroSection.style.display = 'block';
+        listagemSection.style.display = 'none';
+        vagasSection.style.display = 'none';
     });
-    listagemLink.addEventListener("click", function(event) {
+    listagemLink.addEventListener('click', function(event) {
         event.preventDefault();
-        cadastroSection.style.display = "none";
-        listagemSection.style.display = "block";
-        vagasSection.style.display = "none";
+        cadastroSection.style.display = 'none';
+        listagemSection.style.display = 'block';
+        vagasSection.style.display = 'none';
     });
-    vagasLink.addEventListener("click", function(event) {
+    vagasLink.addEventListener('click', function(event) {
         event.preventDefault();
-        cadastroSection.style.display = "none";
-        listagemSection.style.display = "none";
-        vagasSection.style.display = "block";
+        cadastroSection.style.display = 'none';
+        listagemSection.style.display = 'none';
+        vagasSection.style.display = 'block';
     });
 
-    const cadastro = document.getElementById("cadastroForm");
+    const cadastro = document.getElementById('cadastroForm');
     // Fazer cadastro da reserva
     cadastro.addEventListener('submit', function(event) {
         event.preventDefault();
 
         // Capturar os valores do formulário
-        const placa = document.getElementById("placa").value;
-        const proprietario = document.getElementById("proprietario").value;
-        const numeroApartamento = document.getElementById("numeroApartamento").value;
-        const blocoApartamento = document.getElementById("blocoApartamento").value;
-        const modeloVeiculo = document.getElementById("modeloVeiculo").value;
-        const corVeiculo = document.getElementById("corVeiculo").value;
-        const numeroVaga = parseInt(document.getElementById("numeroVaga").value, 10);
+        const placa = document.getElementById('placa').value;
+        const proprietario = document.getElementById('proprietario').value;
+        const numeroApartamento = document.getElementById('numeroApartamento').value;
+        const blocoApartamento = document.getElementById('blocoApartamento').value;
+        const modeloVeiculo = document.getElementById('modeloVeiculo').value;
+        const corVeiculo = document.getElementById('corVeiculo').value;
+        const numeroVaga = parseInt(document.getElementById('numeroVaga').value, 10);
 
         // Criar uma instância da classe Reserva com os valores do formulário
         const reserva = new Reserva(placa, proprietario, numeroApartamento, blocoApartamento, modeloVeiculo, corVeiculo, numeroVaga);
@@ -63,11 +63,12 @@ document.addEventListener("DOMContentLoaded", function() {
         // Exibir uma mensagem de sucesso ou erro
         if (reservaBemSucedida) {
             console.log(reserva);
-            alert("Reserva bem-sucedida! A vaga foi reservada.");
+            alert('Reserva bem-sucedida! A vaga foi reservada.');
             preencherListaReservas(estacionamento);
             preencherListaVagas(estacionamento);
         } else {
-            alert("Falha na reserva. A vaga especificada não está disponível.");
+            alert('Falha na reserva.');
+            return;
         }
         
         // Limpar o formulário
@@ -75,7 +76,7 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     function preencherListaReservas(estacionamento) {
-        const reservasList = document.getElementById("reservasList");
+        const reservasList = document.getElementById('reservasList');
     
         // Limpar a lista antes de preenchê-la novamente
         reservasList.innerHTML = '';
@@ -83,29 +84,44 @@ document.addEventListener("DOMContentLoaded", function() {
         estacionamento.vagas.forEach(vaga => {
             if (vaga.status === 'ocupada') {
                 const reserva = vaga.reserva;
-                const listItem = document.createElement("li");
+                const listItem = document.createElement('li');
                 listItem.textContent = `Vaga ${vaga.numero} - Placa: ${reserva.placa} - Proprietário: ${reserva.proprietario} - Apartamento: ${reserva.numeroApartamento} - Bloco: ${reserva.blocoApartamento} - Modelo: ${reserva.modeloVeiculo} - Cor: ${reserva.corVeiculo}`;
+
+                // Botão para desocupar a vaga
+                const desocuparButton = document.createElement('button');
+                desocuparButton.textContent = 'Desocupar';
+                desocuparButton.classList.add("desocupar-button");
+                desocuparButton.addEventListener('click', function() {
+                    // Desocupando vaga e atualizando listas
+                    estacionamento.desocuparVaga(vaga.numero);
+                    preencherListaVagas(estacionamento)
+                    preencherListaReservas(estacionamento)
+                    alert('Vaga desocupada com sucesso!');
+                });
+
+                listItem.appendChild(desocuparButton);
                 reservasList.appendChild(listItem);
             }
         });
     }
 
     function preencherListaVagas(estacionamento) {
-        const listaVagas = document.getElementById("vagasList");
+        const listaVagas = document.getElementById('vagasList');
     
         // Limpar a lista de vagas antes de preenchê-la novamente
         listaVagas.innerHTML = '';
     
         if (estacionamento.vagas.length > 0) {
             estacionamento.vagas.forEach(vaga => {
-                const listItem = document.createElement("li");
+                const listItem = document.createElement('li');
                 const status = vaga.status === 'disponivel' ? 'Disponível' : 'Ocupada';
                 listItem.textContent = `Vaga ${vaga.numero} - ${status}`;
                 listaVagas.appendChild(listItem);
             });
         } else {
-            listaVagas.textContent = "Nenhuma vaga disponível.";
+            listaVagas.textContent = 'Nenhuma vaga disponível.';
         }
-    }    
+    }
+
     
 });
--- Model/reserva.js
+++ Model/reserva.js
@@ -14,15 +14,25 @@ class Reserva {
         const vagaDisponivel = estacionamento.vagas.find(vaga => vaga.numero === this.numeroVaga && vaga.status === 'disponivel');
 
         if (vagaDisponivel) {
+            // Verifica se a placa já está em alguma reserva
+            const placaExistente = estacionamento.vagas.some(vaga => vaga.status === 'ocupada' && vaga.reserva.placa === this.placa);
+
+            if (placaExistente) {
+                alert('Placa já está em uso em outra reserva.');
+                return false; // Placa já está em uso em outra reserva
+            }
+
             // Ocupa a vaga no estacionamento
             const reservaBemSucedida = estacionamento.ocuparVaga(this.numeroVaga, this);
 
             if (reservaBemSucedida) {
                 return true; // Reserva bem-sucedida
-            } else {
+            } else { 
+                alert('Falha ao ocupar a vaga.');
                 return false; // Falha ao ocupar a vaga
             }
         } else {
+            alert('A vaga já está ocupada.');
             return false; // A vaga especificada não está disponível
         }
     }
